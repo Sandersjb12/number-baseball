d402e8feef27d85e1eecd024cc6df22d5190aaf3
Jeremy Sanders <sandersjb12@gmail.com>
2016-05-05 21:56:46 -0500

    Finished project

---
+++ Game.cs
@@ -0,0 +1,101 @@
+﻿//Jeremy Sanders
+//INSY 4306
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSproject2
+{
+    class Game
+    {
+        private int[] cpu;
+        private int[] player;
+        private int strikes;
+        private int balls;
+        private int turns;
+
+        public Game()
+        {
+            cpu = new int[3];
+            player = new int[3];
+        }
+
+        public void displayCPUNumbers()
+        {
+            Console.Write("CPU Numbers: ");
+            Console.Write(cpu[0]);
+            Console.Write(cpu[1]);
+            Console.WriteLine(cpu[2]);
+        }
+
+        public void generateNumbers()
+        {
+            Random rnd = new Random();
+
+            cpu[0] = rnd.Next(1, 10);
+
+            do
+            {
+                cpu[1] = rnd.Next(1, 10);
+            }
+            while (cpu[1] == cpu[0]);
+
+            do
+            {
+                cpu[2] = rnd.Next(1, 10);
+            }
+            while (cpu[2] == cpu[0] || cpu[2] == cpu[1]);
+        }
+
+        public void playerNumbers()
+        {
+            Console.WriteLine("Enter three numbers, pressing enter after each number:");
+            player[0] = Int32.Parse(Console.ReadLine());
+            player[1] = Int32.Parse(Console.ReadLine());
+            player[2] = Int32.Parse(Console.ReadLine());
+        }
+
+        public bool runTurn()
+        {
+            for (turns = 1; turns < 6; turns++)
+            {
+                strikes = 0;
+                balls = 0;
+                Console.WriteLine("Turn " + turns);
+                playerNumbers();
+
+                Console.Write(player[0]);
+                Console.Write(player[1]);
+                Console.WriteLine(player[2]);
+
+                if (player[0] == cpu[0])
+                    strikes++;
+
+                if (player[1] == cpu[1])
+                    strikes++;
+
+                if (player[2] == cpu[2])
+                    strikes++;
+
+                if (player[0] == cpu[1] || player[0] == cpu[2])
+                    balls++;
+
+                if (player[1] == cpu[0] || player[1] == cpu[2])
+                    balls++;
+
+                if (player[2] == cpu[0] || player[2] == cpu[1])
+                    balls++;
+
+                Console.WriteLine(strikes + " strikes and " + balls + " balls.");
+
+                if (strikes >= 3)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GameMain.cs
@@ -0,0 +1,93 @@
+﻿//Jeremy Sanders
+//INSY 4306
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSproject2
+{
+    class GameMain
+    {
+        static void Main(string[] args)
+        {
+            
+            int wins;
+            int games;
+            double winPct;
+            bool cont;
+            bool won;
+            Game g;
+
+            games = 0;
+            wins = 0;
+            cont = true;
+
+            Console.WriteLine("Number Baseball Game by Jeremy Sanders");
+
+            while (cont == true)
+            {
+                g = new Game();
+
+                won = false;
+
+                g.generateNumbers();
+                g.displayCPUNumbers();
+                won=g.runTurn();
+
+                if (won == true)
+                    wins = win(wins);
+                else
+                    loss();
+
+                games++;
+
+                winPct =(double) wins / games;
+                Console.WriteLine(winPct);
+                Console.WriteLine("{0} wins out of {1} games. Win percentage: {2:0%}.",wins,games,winPct);
+
+                cont = checkContinue();
+
+                Console.WriteLine("Press enter to continue.");
+                Console.ReadLine();
+            }
+        }
+
+        static int win(int w)
+        {
+            Console.WriteLine("You win!");
+            w++;
+            return w;
+        }
+
+        static void loss()
+        {
+            Console.WriteLine("You lose.");
+        }
+
+        static bool checkContinue()
+        {
+            string input;
+
+            while (true)
+            {
+                Console.Write("Continue? (y/n) ");
+                input = Console.ReadLine();
+
+                if (input == "y")
+                    return true;
+
+                else if (input == "n")
+                    return false;
+
+                else
+                    Console.WriteLine("Please enter y or n.");
+            }
+
+            Console.WriteLine("Error: checkContinue()");
+            return false;
+        }
+    }
+}
